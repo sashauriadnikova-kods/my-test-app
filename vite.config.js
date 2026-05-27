b094bf09d4eb053a8de6f79abe25eee6832ed84e
unknown <sashauriadnikova@gmail.com>
2026-05-27 14:34:30 +0300

    Обновление теста

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/my-test-app/',  // ← Добавьте эту строку (замените на точное имя вашего репозитория)
 })
